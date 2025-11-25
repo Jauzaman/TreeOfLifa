1974b6f6066ffcf7da13fe6ed314e5c2daa0a6b3
Josef <tree.of.liifa@gmail.com>
2025-11-25 20:54:05 +0100

    chore: add config.js to .gitignore and remove real Stripe key from committed config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 window.APP_CONFIG = {
     // These should be replaced during build/deployment
-    STRIPE_PUBLISHABLE_KEY: 'pk_live_51QMaGPQ2W4lqdp6bGjycLPoHe8MU18dxSB1Cl24PjI4Asb17IGeSg8ShrOlg1uGOcHPzgiWBh5YCit6dpcDRcjET00rHmOAljm',
+    STRIPE_PUBLISHABLE_KEY: '{{STRIPE_PUBLISHABLE_KEY}}',
     PAYPAL_CLIENT_ID: '{{PAYPAL_CLIENT_ID}}',
     API_BASE_URL: 'https://treeoflifa-production.up.railway.app',
     APP_URL: 'https://treeoflifa-production.up.railway.app'
