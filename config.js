4aea0dd3f6917122bde57508b95fa6015cd08c71
Josef <tree.of.liifa@gmail.com>
2025-11-25 20:50:46 +0100

    chore: set real Stripe publishable key in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 window.APP_CONFIG = {
     // These should be replaced during build/deployment
-    STRIPE_PUBLISHABLE_KEY: '{{STRIPE_PUBLISHABLE_KEY}}',
+    STRIPE_PUBLISHABLE_KEY: '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',
     PAYPAL_CLIENT_ID: '{{PAYPAL_CLIENT_ID}}',
     API_BASE_URL: 'https://treeoflifa-production.up.railway.app',
     APP_URL: 'https://treeoflifa-production.up.railway.app'
