087d3dca10595a1a7d6973892f8b13f1ab62009c
Josef <tree.of.liifa@gmail.com>
2025-11-27 22:35:21 +0100

    Chore: Switch to regex-only email validation and update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 // In production, serve this file dynamically from your backend.
 window.APP_CONFIG = {
   API_BASE_URL: "https://treeoflifa-production.up.railway.app", // Railway backend URL
-  STRIPE_PUBLISHABLE_KEY: "pk_live_51QMaGPQ2W4lqdp6bGjycLPoHe8MU18dxSB1Cl24PjI4Asb17IGeSg8ShrOlg1uGOcHPzgiWBh5YCit6dpcDRcjET00rHmOAljm", // Live key for production
+  STRIPE_PUBLISHABLE_KEY: "pk_test_51QMaGPQ2W4lqdp6blnxdZ4PI582jLSsnjDznejPKX3wSDDQ8PKEQVaZnqPeY22L0pekbSN7nyAt05B7OKqDDPyeg00QM3TiCAA", // Live key for production
   PAYPAL_CLIENT_ID: "BAA7xmM7_mZzcFjQ7TqjlWwvEYrMw0KYVJPdDT-jGnlRHJqJHKh_vOBJOu29cClq-jN10lW-q4yHnyKIcw" // Add PayPal client ID if needed
 };
